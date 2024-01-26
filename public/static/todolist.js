4757ae134de73e25cff9d1d9c304d3f751ced6b5
webdevyoo <yjk7454@naver.com>
2024-01-26 10:30:42 +0900

    feat: 투두리스트js redux 코드 세팅

--- public/static/todolist.js
+++ public/static/todolist.js
@@ -1,6 +1,23 @@
+import { createStore } from "/node_modules/redux/dist/redux.mjs"
+
 const form = document.querySelector("form");
 const input = document.querySelector("input")
 const ul = document.querySelector("ul")
+
+const ADD_TODO="ADD_TODO"
+const DEL_TODO="DEL_TODO"
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return []
+      case DEL_TODO:
+      return []
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer)
 
 const createToDo = toDo => {
   console.log("createToDo")
@@ -14,6 +31,7 @@ const onSubmit = e => {
   const toDo = input.value;
   input.value = "";
   createToDo(toDo)
+  store.dispatch({type:ADD_TODO, data:toDo})
 }
 
 form.addEventListener("submit", onSubmit);
