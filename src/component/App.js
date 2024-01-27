6c426ddfc5196e9241349d8e5a73313e7cdf70b0
webdevyoo <yjk7454@naver.com>
2024-01-27 16:23:38 +0900

    feat: route컴포넌트 뼈대 추가 및 라우팅

---
+++ src/component/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
+
+
+export default function App () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/detail" element={<Detail/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
