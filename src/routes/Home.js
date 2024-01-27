06ea65709913475d44d797ed3bee7a6eef33bb03
webdevyoo <yjk7454@naver.com>
2024-01-28 02:07:38 +0900

    feat: home컴포넌트 뼈대 구현 및 redux store dispatch적용

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,29 @@
-export default function Home () {return "Home"}
+import { useState } from "react";
+import store from "../redux/store";
+
+function Home () {
+
+  const [text, setText] = useState("")
+
+  function onChange(e) {
+    setText(e.target.value)
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    store.dispatch({type:"ADD"})
+    setText("")
+  }
+
+  return (
+  <>
+    <h1>To Do</h1>
+    <form onSubmit={onSubmit}>
+      <input type="text" value={text} onChange={onChange}/>
+      <button>Add</button>
+    </form>
+    <ul></ul>
+  </>
+  )
+}
+
+export default Home;
