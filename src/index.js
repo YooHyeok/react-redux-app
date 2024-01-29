d237eb6190540cac05605c14c769056c8f377da1
webdevyoo <yjk7454@naver.com>
2024-01-29 10:20:03 +0900

    feat: index.js - Provider로 store 전역 공급

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from "react-dom/client"
 import App from "./component/App"
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM 
       .createRoot(document.getElementById('root'))
-      .render(<App/>)
+      .render(
+            <Provider store={store}>
+                  <App/>
+            </Provider>
+      )
