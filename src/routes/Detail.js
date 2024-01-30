94ff03cc4458fdea0b3a45ce6f8df346022680e7
webdevyoo <yjk7454@naver.com>
2024-01-30 10:23:52 +0900

    fix: Detail컴포넌트 id와 data 출력

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,12 +3,15 @@ import { useParams } from "react-router-dom"
 
 
 function mapStateProps(state, ownProps) {
-  console.log(ownProps)
-  console.log(state)
   return {state}
 }
 export default connect(mapStateProps) (function Detail ({state}) {
   const {id} = useParams()
-  console.log(state.find(todo=>todo.id === parseInt(id)))
-  return <h1>{state.find(todo=> todo.id === parseInt(id)).data}</h1>
+  const toDo = state.find(todo=> todo.id === parseInt(id))
+  return (
+    <>
+      <h1>{toDo?.data}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  )
 })
