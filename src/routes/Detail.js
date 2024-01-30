ee428791e0cfcefeb3e26f1b170e76db0b654d22
webdevyoo <yjk7454@naver.com>
2024-01-30 10:17:25 +0900

    feat: Detail 컴포넌트 내용 구현
    mapStateProps로 state props id와 일치하는 data값 출력

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,14 @@
-export default function Detail () {
-  return "Detail"
+import { connect } from "react-redux"
+import { useParams } from "react-router-dom"
+
+
+function mapStateProps(state, ownProps) {
+  console.log(ownProps)
+  console.log(state)
+  return {state}
 }
+export default connect(mapStateProps) (function Detail ({state}) {
+  const {id} = useParams()
+  console.log(state.find(todo=>todo.id === parseInt(id)))
+  return <h1>{state.find(todo=> todo.id === parseInt(id)).data}</h1>
+})
