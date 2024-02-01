9013d0f987c2d65bfbfd95726fd6eeb7f3861479
webdevyoo <yjk7454@naver.com>
2024-02-01 11:39:54 +0900

    feat: createSlice로 reducer적용

--- src/redux/storeNew.js
+++ src/redux/storeNew.js
@@ -1,26 +1,39 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit"
+import { configureStore, createAction, createReducer, createSlice } from "@reduxjs/toolkit"
 
-const ADD = "ADD"
-const DELETE = "DELETE"
-
+/* 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
 
 const reducer = createReducer([], (builder) => {
   builder
   .addCase(addToDo,(state, action) => {state.push({id: Date.now(), data: action.payload})})
   .addCase(deleteToDo,(state, action) => state.filter(el=> el.id !== action.payload))
   }
-)
+) 
 
 const store = configureStore({reducer})
-
-// store.subscribe()
 
 export const actionCreator = {
   addToDo,
   deleteToDo
+}
+*/
+
+const toDos = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    addToDo: (state, action) => {state.push({id: Date.now(), data: action.payload})},
+    deleteToDo: (state, action) => state.filter(el=> el.id !== action.payload)
+  }
+})
+console.log(toDos)
+
+const store = configureStore({reducer: toDos.reducer})
+
+export const actionCreator = {
+  addToDo: toDos.actions.addToDo,
+  deleteToDo: toDos.actions.deleteToDo
 }
 
 export default store;
